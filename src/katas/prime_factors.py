2d5a105b01f86e1cf7b4b8310b92e2dfaae412ca
thavarshan <tjthavarshan@gmail.com>
2020-07-22 21:52:55 +0530

    Initial commit

---
+++ src/katas/prime_factors.py
@@ -0,0 +1,15 @@
+class PrimeFactors():
+
+    def generate(self, number):
+        factors = []
+        divisor = 2
+
+        while number > 1:
+            while number % divisor == 0:
+                factors.append(divisor)
+
+                number = number / divisor
+
+            divisor+=1
+
+        return factors
---
+++ tests/prime_factors_test.py
@@ -0,0 +1,31 @@
+from src.katas import prime_factors
+from unittest_data_provider import data_provider
+import unittest
+
+
+class PrimeFactorsTest(unittest.TestCase):
+
+    def setUp(self):
+        self.generator = prime_factors.PrimeFactors()
+
+    def checks():
+        return [
+            [1, []],
+            [2, [2]],
+            [3, [3]],
+            [4, [2, 2]],
+            [5, [5]],
+            [6, [2, 3]],
+            [8, [2, 2, 2]],
+            [9, [3, 3]],
+            [100, [2, 2, 5, 5]]
+        ]
+
+    @data_provider(checks)
+    def test_it_generates_prime_factors(self, number, factors):
+        self.assertEqual(factors, self.generator.generate(number))
+
+
+
+if __name__ == '__main__':
+    unittest.main()
